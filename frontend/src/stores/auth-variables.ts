944415bff5f255896332f5a149999b55bc18ffc0
Patrick Grosslicht <patrick@grosslicht.com>
2019-04-08 07:53:24 +0200

    fix: change callback url

--- frontend/src/stores/auth-variables.ts
+++ frontend/src/stores/auth-variables.ts
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   audience: 'https://burgertuesday.rocks',
-  callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'https://app.burgertuesday.rocks/callback',
   clientId: 'skDe4MqsVC6BSn6DbrbRC5ZEgEqniWwT',
   domain: 'burgertuesday.eu.auth0.com',
 };
