a891369418867fde6f30a441cb629db0fd122820
Patrick Grosslicht <patrick@grosslicht.com>
2019-04-09 07:26:43 +0200

    chore: build pull requests on travis

---
+++ backend/ci/build_and_deploy.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./gradlew --no-daemon build jib
---
+++ frontend/ci/build_and_deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo "$DOCKER_PASSWORD" | docker login -u "pdgwien" --password-stdin
+yarn test
+docker build -t "$CONTAINER_NAME:latest" .
+docker tag "$CONTAINER_NAME:latest" "$CONTAINER_NAME:$TRAVIS_BUILD_NUMBER" && docker push "$CONTAINER_NAME"
