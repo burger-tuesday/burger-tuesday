125f88ad7c11899f7fe169239b759e16a9718831
Patrick Grosslicht <patrick@grosslicht.com>
2019-04-29 20:04:22 +0200

    fix: fix type definition of constants

--- frontend/src/stores/auth-variables.ts
+++ frontend/src/stores/auth-variables.ts
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
-  audience: process.env.REACT_APP_AUTH_AUDIENCE,
-  callbackUrl: process.env.REACT_APP_AUTH_CALLBACK_URL,
-  clientId: process.env.REACT_APP_AUTH_CLIENT_ID,
-  domain: process.env.REACT_APP_AUTH_DOMAIN
+  audience: process.env.REACT_APP_AUTH_AUDIENCE || '',
+  callbackUrl: process.env.REACT_APP_AUTH_CALLBACK_URL || '',
+  clientId: process.env.REACT_APP_AUTH_CLIENT_ID || '',
+  domain: process.env.REACT_APP_AUTH_DOMAIN || ''
 };
--- frontend/src/stores/constants.ts
+++ frontend/src/stores/constants.ts
@@ -1,3 +1,3 @@
 export const CONSTANTS = {
-  API_URL: process.env.REACT_APP_API_URL
+  API_URL: process.env.REACT_APP_API_URL || ''
 };
