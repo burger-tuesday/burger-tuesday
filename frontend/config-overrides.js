fcaa127e977d68f900c16b062e27696a0c611ef4
Patrick Grosslicht <patrick@grosslicht.com>
2019-04-29 20:16:38 +0200

    fix: fix sentry webpack not finding sources

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,9 +1,12 @@
+const SentryCliPlugin = require('@sentry/webpack-plugin');
+
 module.exports = {
   webpack: function (config, env) {
     if (process.env.CI) {
       config.plugins = (config.plugins || [])
-        .concat([new SentryCliPlugin({})]);
+      .concat([new SentryCliPlugin({include: '.'})]);
     }
+
     return config;
   }
-}
+};
