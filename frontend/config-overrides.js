08fdba443b4674842edbe44d6e8647e8e38d1c48
Patrick Grosslicht <patrick@grosslicht.com>
2019-04-29 20:32:18 +0200

    fix: fix environment variables not being passed through to docker

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -2,9 +2,9 @@ const SentryCliPlugin = require('@sentry/webpack-plugin');
 
 module.exports = {
   webpack: function (config, env) {
-    if (process.env.CI) {
+    if (process.env.UPLOAD_SOURCEMAPS) {
       config.plugins = (config.plugins || [])
-      .concat([new SentryCliPlugin({include: '.'})]);
+        .concat([new SentryCliPlugin({include: '.'})]);
     }
 
     return config;
